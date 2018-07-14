de0ce3e55124971f00e55707cc4f338ac47a5770
Chris Cardle <cdcardle@gmail.com>
2018-07-14 17:12:38 +0000

    automatically backed up by learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,10 +1,10 @@
 class StoreAdminController < ApplicationController
   layout "admin"
-  
+
   def home
   end
 
-  def order
-    
+  def orders
+    render :layout => "order_administration"
   end
 end
